bb5e61c39c21eb4a61657799ae427ddf1610f2e5
k.takayanagi <kazumi-takayanagi@maemzou.com>
2022-09-20 01:01:30 +0900

    途中コミット

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -6,110 +11,128 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginPageState extends State<LoginPage> {
+  List items = [];
+  bool isError = false;
+  String errorString = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<void> getLoginResponse2() async {
+    // final Response response = await Dio().get(
+    //   'https://userpageuat.azurewebsites.net/api/Login',
+    //   queryParameters: {
+    //     'code': 'iTAx69vL/UuhBflxh82uUiwSd1XaFAneFbFWs/OhpJT5jKgzK85vbg==',
+    //     'env': 'uat',
+    //     'userId': 'kazumi.takayanagi@mtlo.co.jp',
+    //     'passWord': 'Mamezou@001',
+    //   },
+    // );
+
+    final Response response = await Dio().get(
+        'https://userpageuat.azurewebsites.net/api/Login?code=iTAx69vL/UuhBflxh82uUiwSd1XaFAneFbFWs/OhpJT5jKgzK85vbg==&env=uat&userId=kazumi.takayanagi@mtlo.co.jp&passWord=Mamezou@001');
+
+    print('ErrorCode:');
+    print(response.data['ErrorCode']);
+    print('RetMessage:');
+    print(response.data['RetMessage']);
+    print('token:');
+    print(response.data['token']);
+
+    setState(() {}); // 画面を更新したいので setState も呼んでおきます
+  }
+
+  Future<void> getLoginResponse() async {
+    try {
+      // 1. GetでResponseを取得
+      var response = await http
+          .get(Uri.https('userpageuat.azurewebsites.net', '/api/Login', {
+        'code': 'iTAx69vL/UuhBflxh82uUiwSd1XaFAneFbFWs/OhpJT5jKgzK85vbg==',
+        'env': 'uat',
+        'userId': 'kazumi.takayanagi@mtlo.co.jp',
+        'passWord': 'Mamezou@001',
+      }));
+      // 2. 問題がなければ、Json型に変換したデータを格納
+      var jsonResponse = _response(response);
+
+      print('ErrorCode:');
+      print(jsonResponse['ErrorCode']);
+      print('RetMessage:');
+      print(jsonResponse['RetMessage']);
+      print('token:');
+      print(jsonResponse['token']);
+
+      var userInfo = jsonResponse['UserInfo'];
+      print(userInfo['userId']);
+
+      setState(() {}); // 画面を更新したいので setState も呼んでおきます
+
+      // throw Exception();
+    } on SocketException catch (socketException) {
+      // ソケット操作が失敗した時にスローされる例外
+      debugPrint("Error: ${socketException.toString()}");
+      isError = true;
+    } on Exception catch (exception) {
+      // statusCode: 200以外の場合
+      debugPrint("Error: ${exception.toString()}");
+      isError = true;
+    } catch (_) {
+      debugPrint("Error: 何かしらの問題が発生しています");
+      isError = true;
+    }
+  }
+
+  dynamic _response(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        var responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        // 400 Bad Request : 一般的なクライアントエラー
+        throw Exception('一般的なクライアントエラーです');
+      case 401:
+        // 401 Unauthorized : アクセス権がない、または認証に失敗
+        throw Exception('アクセス権限がない、または認証に失敗しました');
+      case 403:
+        // 403 Forbidden ： 閲覧権限がないファイルやフォルダ
+        throw Exception('閲覧権限がないファイルやフォルダです');
+      case 404:
+        // 404 not found ： 見つからない
+        throw Exception('codeが違う可能性あり');
+      case 500:
+        // 500 何らかのサーバー内で起きたエラー
+        throw Exception('何らかのサーバー内で起きたエラーです');
+
+      default:
+        // それ以外の場合
+        throw Exception('何かしらの問題が発生しています');
+    }
+  }
+
+  // この関数の中の処理は初回に一度だけ実行されます。
+  @override
+  void initState() {
+    super.initState();
+
+    // Loginレスポンスを取得します。
+    getLoginResponse();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return Scaffold();
   }
 }
